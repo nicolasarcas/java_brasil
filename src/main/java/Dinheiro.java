6a9101552ebdcafef96c78caf2a2c48047520d9c
= <nicolas.arcas1@gmail.com>
2021-12-22 16:54:17 -0300

    Final aula 04

--- src/main/java/Dinheiro.java
+++ src/main/java/Dinheiro.java
@@ -1,8 +1,12 @@
+import br.com.caelum.stella.inwords.FormatoDeReal;
+import br.com.caelum.stella.inwords.NumericToWordsConverter;
 import org.javamoney.moneta.Money;
+import org.javamoney.moneta.function.MonetaryOperators;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
+import javax.money.NumberValue;
 
 public class Dinheiro {
     public static void main(String[] args) {
@@ -11,5 +15,14 @@ public class Dinheiro {
         MonetaryAmount valorDaParcela = Money.of(75.15, moeda);
 
         System.out.println(valorDaParcela);
+
+        MonetaryAmount valorTotal = valorDaParcela.multiply(12);
+        System.out.println(valorTotal);
+        MonetaryAmount desconto =  valorTotal.with(MonetaryOperators.percent(10));
+        System.out.println(desconto);
+        NumberValue descontoSemMoeda = desconto.getNumber();
+        NumericToWordsConverter converter = new NumericToWordsConverter(new FormatoDeReal());
+        String valorExtenso = converter.toWords(descontoSemMoeda.doubleValue());
+        System.out.println(valorExtenso);
     }
 }
